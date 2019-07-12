6e7e58ef10b820ad28fc71fbe4888648e7c60642
Gabriel Vitor <gxvitor.1997@hotmail.com>
2019-07-11 23:47:37 -0300

    #Add arquivos

---
+++ src/config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+  uri:
+    "mongodb+srv://admin:admin@cluster0-dkslq.mongodb.net/test?retryWrites=true&w=majority"
+};
---
+++ src/routes.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+const routes = express.Router();
+
+const authMiddleware = require("./app/middlewares/auth");
+
+const controllers = require("./app/controllers");
+
+/**
+ * Rotas de cadastro e login
+ */
+routes.post("/users", controllers.UserController.store);
+routes.post("/sessions", controllers.SessionController.store);
+
+/**
+ * Middleware de autenticação
+ */
+routes.use(authMiddleware);
+
+/**
+ * Ads
+ */
+routes.get("/ads", controllers.AdController.index);
+routes.get("/ads/:id", controllers.AdController.show);
+routes.post("/ads", controllers.AdController.store);
+routes.put("/ads/:id", controllers.AdController.update);
+routes.delete("/ads/:id", controllers.AdController.destroy);
+
+/**
+ * Purchases
+ */
+routes.post("/purchases", controllers.PurchaseController.store);
+
+module.exports = routes;
